22129a26f7d219cb812dfd11eae0d605edca1156
Brandi Cameron <hello@brandicameron.com>
2021-04-12 14:40:04 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+let bill = document.getElementById("bill");
+let tipValue = 25;
+let personValue = 2;
+
+function tipAmount(e) {
+  e.preventDefault();
+  let tipOutput = document.querySelector(".tip-output");
+  if (e.target.classList.contains("tip-decrement") && tipValue === 0) {
+    tipValue = 0;
+    tipOutput.textContent = tipValue;
+  } else if (e.target.classList.contains("tip-decrement")) {
+    tipValue = tipValue - 5;
+    tipOutput.textContent = tipValue;
+  } else if (e.target.classList.contains("tip-increment")) {
+    tipValue = tipValue + 5;
+    tipOutput.textContent = tipValue;
+  }
+}
+
+function personCounter(e) {
+  e.preventDefault();
+  let personOutput = document.querySelector(".person-output");
+  if (e.target.classList.contains("person-decrement") && personValue === 0) {
+    personValue = 0;
+    personOutput.textContent = personValue;
+  } else if (e.target.classList.contains("person-decrement")) {
+    personValue = personValue - 1;
+    personOutput.textContent = personValue;
+  } else if (e.target.classList.contains("person-increment")) {
+    personValue = personValue + 1;
+    personOutput.textContent = personValue;
+  }
+}
+
+function calculateBill() {
+  let billAmount = parseFloat(bill.value);
+  let totalTip = document.querySelector(".total-tip");
+  let totalBill = document.querySelector(".total-bill");
+  let personTip = document.querySelector(".person-tip");
+  let personBill = document.querySelector(".person-bill");
+  let tipTotal = parseFloat((billAmount * tipValue) / 100);
+  let billTotal = billAmount + tipTotal;
+
+  if (isNaN(billAmount)) {
+    totalTip.textContent = `0.00`;
+    totalBill.textContent = `0.00`;
+    personTip.textContent = `0.00`;
+    personBill.textContent = `0.00`;
+  } else {
+    totalTip.textContent = tipTotal.toFixed(2);
+    totalBill.textContent = billTotal.toFixed(2);
+    personTip.textContent = (tipTotal / personValue).toFixed(2);
+    personBill.textContent = (billTotal / personValue).toFixed(2);
+  }
+}
+
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", tipAmount);
+  button.addEventListener("click", personCounter);
+  button.addEventListener("click", calculateBill);
+});
+
+bill.addEventListener("input", calculateBill);
